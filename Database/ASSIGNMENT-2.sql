f9637c9e06de292cd4b61260e3b02e7d03488a44
tanvi7122 <tanvizankat@gmail.com>
2023-02-07 11:49:42 +0530

    COMMIT

--- Database/ASSIGNMENT-2.sql
+++ Database/ASSIGNMENT-2.sql
@@ -3,5 +3,5 @@ select * from customer
 select* from salesman
 
 --write a SQL query to find the salesperson and customer who reside in the same city. Return Salesman, cust_name and city
-select c.cust,s.name,c.city from customer c join salesman s
+select c.cust_name,s.name,c.city from customer c join salesman s
 on customer.city=salesman.city;

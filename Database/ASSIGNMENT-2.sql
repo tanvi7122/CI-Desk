87278500d4b63ba932805c149a615f180be27060
tanvi7122 <81165160+tanvi7122@users.noreply.github.com>
2023-02-08 15:02:38 +0530

    delete

--- Database/ASSIGNMENT-2.sql
+++
@@ -1,7 +0,0 @@
-select *from orders
-select * from customer
-select* from salesman
-
---write a SQL query to find the salesperson and customer who reside in the same city. Return Salesman, cust_name and city
-select c.cust_name,s.name,c.city from customer c join salesman s
-on customer.city=salesman.city;
